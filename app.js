1f3c0051cae214b0db659091cf8fdd2a318d7e6f
Harold Sikkema <harold@nsitu.ca>
2026-06-25 10:10:33 -0400

    add basic Express server setup with port configuration

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
     res.send("Hello from Express");
+}); 
+
+app.listen(port, "127.0.0.1", () => {
+  console.log(`Listening on ${port}`);
 });
